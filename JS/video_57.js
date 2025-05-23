e73f5ea2ce5059b1fb288d1f060a9157dda8d300
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-05-23 11:33:14 +0530

    on going

---
+++ JS/video_57.js
@@ -0,0 +1,14 @@
+let emp = {
+    name: "swastik",
+    role: "developer",
+    salary: 50000
+}
+ for (const key in emp) {
+        const element = emp[key];
+        console.log(key,element);
+    }
+ 
+for (const c of "swastik") {
+    console.log(c)
+    
+}
