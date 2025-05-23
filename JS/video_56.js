88c6a156ad329e5ab5d4aab89ae9e7491d2c54bd
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-05-23 09:16:00 +0530

    need more pratice.

---
+++ JS/video_56.js
@@ -0,0 +1,15 @@
+age = 20;
+
+if (age >= 18) {
+    console.log("you can drive car");
+} else{
+    console.log("you cannot drive car");
+    
+}
+
+if(age >= 10 && age <= 20){
+    console.log("you are aligiable");
+} else{
+    console.log("you are not");
+    
+}
