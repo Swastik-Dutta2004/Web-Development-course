1119190093ed684993c03387d0b1d8c79c76d6b6
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-05-23 11:37:40 +0530

    on going.

--- JS/video_57.js
+++ JS/video_57.js
@@ -1,14 +1,27 @@
-let emp = {
-    name: "swastik",
-    role: "developer",
-    salary: 50000
-}
- for (const key in emp) {
-        const element = emp[key];
-        console.log(key,element);
-    }
+// let emp = {
+//     name: "swastik",
+//     role: "developer",
+//     salary: 50000
+// }
+//  for (const key in emp) {
+//         const element = emp[key];
+//         console.log(key,element);
+//     }
  
-for (const c of "swastik") {
-    console.log(c)
+// for (const c of "swastik") {
+//     console.log(c)
     
-}
+// }
+
+// let s = 0;
+// while (s<10) {
+//     console.log(s);
+//     s++
+    
+// }
+
+let s = 0;
+do {
+    console.log(s);
+    s++;
+} while (s<10);
