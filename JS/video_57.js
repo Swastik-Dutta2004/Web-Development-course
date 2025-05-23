5c991d4a2a650574d744d22853dc86d85c0af811
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-05-23 18:30:12 +0530

    trying to give my best.

--- JS/video_57.js
+++ JS/video_57.js
@@ -20,8 +20,28 @@
     
 // }
 
-let s = 0;
-do {
-    console.log(s);
-    s++;
-} while (s<10);
+// let s = 0;
+// do {
+//     console.log(s);
+//     s++;
+// } while (s<10);
+
+// let marks = [89, 56, 87, 56, 79];
+
+// console.log("Marks of the student1 are:");
+
+// let subjectNumber = 1; // Start from Subject 1
+
+// for (const mark of marks) {
+//   console.log("Subject " + subjectNumber + ": " + mark);
+//   subjectNumber++; // Move to the next subject number
+// }
+
+let marks = [89, 56, 87, 56, 79];
+
+console.log("Marks of the student1 are:");
+
+for (const key in marks) {
+        const element = marks[key];
+        
+    }
---
+++ JS/video_58.js
@@ -0,0 +1,28 @@
+// function HAUSTLE(name){
+//     console.log("hey " +name+ " you are wasting your time so much");
+//     console.log("hey " + name + " please stop wasting your time");
+//     console.log("hey " + name + " please return to your earlier from");
+//     console.log("hey "+ name + " i know you can do it");
+//     console.log("otherwise you will unable to fullfill your dreams");
+// }
+// HAUSTLE("swastik")
+
+// function sum(a ,b){
+//     return a+b;
+// }
+// result1 = sum(3,6)
+// result2 = sum(57,90)
+// result3 = sum(37,69)
+
+// console.log("the result of the sum is:", result1);
+// console.log("the result of the sum is:", result2);
+// console.log("the result of the sum is:", result3);
+
+
+const funct1 = (x) => {
+    console.log("i am a arrow function",x);
+}
+
+funct1(57);
+funct1(455)
+
