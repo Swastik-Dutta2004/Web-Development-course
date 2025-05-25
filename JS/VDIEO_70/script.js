0f1cc92bc1d8e87e2507612e4f43bb64056587ce
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-05-25 12:42:38 +0530

    done the other exercise.

--- JS/VDIEO_70/script.js
+++ JS/VDIEO_70/script.js
@@ -1,2 +1,9 @@
 let arr = ["red","blue","yellow","pink","green"]
 
+let color = document.querySelectorAll(".box")
+
+color.forEach(e => {
+    let random =  arr[Math.floor(Math.random() * arr.length)];
+
+    e.style.backgroundColor = random
+});
