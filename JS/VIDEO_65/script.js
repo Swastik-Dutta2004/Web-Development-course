d98ba2ef2f4e07d226a1bcf4f20ff77248e64aac
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-05-25 12:01:12 +0530

    trying to solving  another problem.

--- JS/VIDEO_65/script.js
+++ JS/VIDEO_65/script.js
@@ -1,9 +1,11 @@
 let number = prompt("Enter a number")
 let fact = 1
+if (number == 0) {
+    console.log("give some positive number💀");
+}else{
 for (let i = 1; i <= number; i++) {
     fact *= i;
     console.log(fact);
     
 }
-
-
+}
