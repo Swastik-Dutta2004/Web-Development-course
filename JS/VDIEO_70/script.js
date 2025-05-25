676ff3bf9bfaf64d4010e07ce9d1df71f8218be5
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-05-25 17:49:17 +0530

    try to say in the routine

--- JS/VDIEO_70/script.js
+++ JS/VDIEO_70/script.js
@@ -1,16 +1,16 @@
-let arr = ["red","blue","yellow","pink","green"]
+let arr = ["red","blue","yellow","pink","green","white","violet"]
 
 let color = document.querySelectorAll(".box")
 
-// color.forEach(e => {
-//     let random =  arr[Math.floor(Math.random() * arr.length)];
+color.forEach(e => {
+    let random =  arr[Math.floor(Math.random() * arr.length)];
 
-//     e.style.backgroundColor = random
-// });
+    e.style.backgroundColor = random
+});
 
-for (let i = 0; i < arr.length; i++) {
-let random = arr[Math.floor(Math.random() * arr.length)]
+// for (let i = 0; i < arr.length; i++) {
+// let random = arr[Math.floor(Math.random() * arr.length)]
 
 
-color[i].style.backgroundColor = random    
-}
+// color[i].style.backgroundColor = random    
+// }
