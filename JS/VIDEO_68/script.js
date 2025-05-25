bcdb141c6985c9c9fc18716eb0883dbb9c4aa706
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-05-25 09:22:37 +0530

    learn a new things by myself.😊

--- JS/VIDEO_68/script.js
+++ JS/VIDEO_68/script.js
@@ -1,4 +1,15 @@
 let boxes = document.getElementsByClassName("box")
 console.log(boxes);
 
-document.getElementById("swa").style.background  = "red"
+// document.getElementById("swa").style.backgroundColor  = "red"
+
+// document.querySelector(".box").style.backgroundColor = "blue"
+
+
+document.querySelectorAll(".box").forEach((e,i) =>{
+    if (i==2) {
+        e.style.backgroundColor  = "red"
+    }else{
+    e.style.backgroundColor = "green";
+    }
+})
