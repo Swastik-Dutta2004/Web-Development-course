cbed2c55db341a915c75ec06c4e7b4d1dbf32511
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-05-27 08:46:44 +0530

    completed

--- JS/VIDEO_73/script.js
+++ JS/VIDEO_73/script.js
@@ -1,13 +1,20 @@
-function cardMaking (image, title, cName, time, views){
+function cardMaking (image, title, cName, time, views, duration){
     document.querySelector(".image").src = image;
     document.querySelector(".title").innerHTML = title
     document.querySelector(".cName").innerHTML = cName
     document.querySelector(".time").innerHTML = time + " months ago"; 
-   if (views >  1000000) {
-    return views += views + "M"
-   } else if (views >10000 ){
-    return views 
-   }
 
+   let formattedViews = "";
+  if (views >= 1000000) {
+    formattedViews = (views / 1000000).toFixed(1) + "M";  // e.g., 1500000 => 1.5M
+  } else if (views >= 1000) {
+    formattedViews = (views / 1000).toFixed(1) + "K";      // e.g., 54000 => 54.0K
+  } else {
+    formattedViews = views;                     // e.g., 800 => 800
+  }
+    document.querySelector(".views").innerHTML = formattedViews;
+
+    document.querySelector(".duration").innerHTML = duration
 }
-cardMaking("https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw","Introduction to Backend | Sigma Web Dev veido #2","Code With Harry","7","54000")
+
+cardMaking("https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw","Introduction to Backend | Sigma Web Dev video #2","Code With Harry","7","5400000","32:21")
