900ba6d8798ac9b8e325379d5eab93e8ca672a42
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-05-27 08:16:59 +0530

    allmost completed

--- JS/VIDEO_73/script.js
+++ JS/VIDEO_73/script.js
@@ -1,7 +1,13 @@
 function cardMaking (image, title, cName, time, views){
     document.querySelector(".image").src = image;
-    document.getElementsByClassName("title").innerHTML = title
-    document.getElementsByClassName("cName").innerHTML = cName
-    document.getElementsByClassName("time").innerHTML = time
-    document.getElementsByClassName("views").innerHTML = views
+    document.querySelector(".title").innerHTML = title
+    document.querySelector(".cName").innerHTML = cName
+    document.querySelector(".time").innerHTML = time + " months ago"; 
+   if (views >  1000000) {
+    return views += views + "M"
+   } else if (views >10000 ){
+    return views 
+   }
+
 }
+cardMaking("https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw","Introduction to Backend | Sigma Web Dev veido #2","Code With Harry","7","54000")
