0db848046f03a7a3d33a45f538fa46caa7a00524
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-05-28 08:25:41 +0530

    promises completed for now

--- JS/VIDOE_75/promise.js
+++ JS/VIDOE_75/promise.js
@@ -1,4 +1,4 @@
-let porm1 = new Promise((resolve,reject) =>{
+let prom1 = new Promise((resolve,reject) =>{
     let a = Math.random()
     if (a < 0.5) {
         reject("NO it cann't be");
@@ -10,8 +10,21 @@ let porm1 = new Promise((resolve,reject) =>{
     } 
 })
 
-porm1.then((a) =>{
-    console.log(a); 
-}).catch((err) => {
-    console.log(err);
+let prom2 = new Promise((resolve,reject) =>{
+    let a = Math.random()
+    if (a < 0.5) {
+        reject("NO it cann't be 2");
+    } else{
+    setTimeout(() => {
+        console.log("yes i am done 2");
+        resolve("swastik 2")
+    }, 2000);
+    } 
+})
+
+let p3 = Promise.reject([prom1,prom2]).then (e => {
+    console.log(e);
+}).catch(arr => {
+    console.log(arr);
+    
 })
