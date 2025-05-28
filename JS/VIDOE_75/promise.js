13f8ad25f706c4e5e47760205ba071f478af78a5
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-05-28 07:36:59 +0530

    promises ongiong

--- JS/VIDOE_75/promise.js
+++ JS/VIDOE_75/promise.js
@@ -1,11 +1,17 @@
 let porm1 = new Promise((resolve,reject) =>{
+    let a = Math.random()
+    if (a < 0.5) {
+        reject("NO it cann't be");
+    } else{
     setTimeout(() => {
-        console.log("yes i am done");
+        console.log("yes i am done ");
         resolve("swastik")
-    },2000)
+    }, 2000);
+    } 
 })
 
 porm1.then((a) =>{
-    console.log(a);
-    
+    console.log(a); 
+}).catch((err) => {
+    console.log(err);
 })
