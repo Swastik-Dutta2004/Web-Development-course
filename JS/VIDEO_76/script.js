4b7cf4582d12ef5ca52f0865f0810f62c8fd23ba
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-05-28 13:42:56 +0530

    promises is completed but needs more pratices

--- JS/VIDEO_76/script.js
+++ JS/VIDEO_76/script.js
@@ -1,20 +1,31 @@
-function getData (){
-    return new Promise ((resolve, reject) => {
-        setTimeout(() => {
-            resolve(455)
-        },3000)
-    })
-}
+// async function getData (){
+//     return new Promise ((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve(455)
+//         },3000)
+//     })
+// }
 
-console.log("loading modules");
-
-console.log("Do Sommething else");
-
-console.log("Load data");
-
-let data =  getData().then((data) =>{
+async function getData (){
+    let x = await fetch ('https://jsonplaceholder.typicode.com/todos/1')
+    let data = await x.json()
     console.log(data);
     
-})
+}
 
+async function main() {
+    console.log("loading modules");
 
+    console.log("Do Sommething else");
+
+    console.log("Load data");
+
+    let data =  await getData()
+        // console.log(data);
+        
+    console.log("process data");
+    
+    console.log("task 2");
+    }
+
+main()
