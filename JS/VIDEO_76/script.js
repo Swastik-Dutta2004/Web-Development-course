b4608e291564c41468345384c359f7233f5a4a07
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-05-28 09:32:12 +0530

    onging

---
+++ JS/VIDEO_76/script.js
@@ -0,0 +1,19 @@
+function getData (){
+    return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            resolve(455)
+        },3000)
+    })
+}
+
+console.log("loading modules");
+
+console.log("Do Sommething else");
+
+console.log("Load data");
+
+let data =  getData()
+console.log(data);
+
+
+
