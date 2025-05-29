c733d839de3b84f9f41531cf086f52329e53b2e4
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-05-29 09:00:26 +0530

    🤝🤝

---
+++ JS/VIDOE_75/pratice2.js
@@ -0,0 +1,13 @@
+console.log("i am done");
+
+let prom1 = new Promise ((resolve, reject) => {
+    setTimeout(() =>{
+        console.log("yes all is ok");
+        resolve("swastik")
+    },2000)
+})
+
+prom1.then((a) => {
+    console.log(a);
+    
+})
