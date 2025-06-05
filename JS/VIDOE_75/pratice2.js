492402141185dd539eccfab492777e8935576116
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-06-05 11:26:18 +0530

    4th commit

--- JS/VIDOE_75/pratice2.js
+++ JS/VIDOE_75/pratice2.js
@@ -1,7 +1,7 @@
 let pro1 = new Promise ((resolve,reject) => {
     let a = Math.random()
         if (a<0.5) {
-           reject("It cannot supports");
+           reject("It cannot supports ");
            console.log(a);
 
         } else{
@@ -12,9 +12,24 @@ let pro1 = new Promise ((resolve,reject) => {
     }
 })
 
-pro1.then((arg) => {
-    console.log(arg);  
-}).catch((err) =>{
-    console.log(err);
-    
+let pro2 = new Promise ((resolve,reject) => {
+    let a = Math.random()
+        if (a<0.5) {
+           reject("It cannot supports 2");
+           console.log(a);
+
+        } else{
+        setTimeout(() => {
+            console.log("yes It done 2");
+            resolve("swastik 2")
+        },2000)
+    }
 })
+
+let pro3 = Promise.all([pro1,pro2])
+    pro3.then((arg) => {
+        console.log(arg);  
+    }).catch((err) =>{
+        console.log(err);
+        
+    })
