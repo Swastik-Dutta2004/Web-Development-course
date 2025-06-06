0fabc2a1465c3715be670308d17621420eb24ed6
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-06-06 09:08:02 +0530

    today 1st commit

---
+++ JS/VIDEO_76/pratice.js
@@ -0,0 +1,28 @@
+// async function getData() {
+//     return new Promise ((resolve,reject) => {setTimeout(() => {
+//         resolve(455)
+//     }, 3000);})
+// }
+
+async function getData() {
+    let x = await fetch ('https://jsonplaceholder.typicode.com/todos/1')
+    let Data = await x.json()
+    return Data    
+}
+
+async function main() {
+    console.log("Loading Function");
+
+    console.log("do something else");
+
+    let a = await getData()
+    console.log(a);
+    
+
+    console.log("Processing Data");
+
+    console.log("showing Data");
+    
+}
+
+main()
