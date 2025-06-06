8e9d2508cfba06e0ad37a91a288f48c402c68b86
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-06-06 11:16:23 +0530

    4th commit

--- JS/VIDEO_79/script.js
+++ JS/VIDEO_79/script.js
@@ -1,7 +1,15 @@
 let a = prompt("Enter the number")
 
 let b = prompt("Enter the number")
+if (isNaN(a) || isNaN(b)){
+    throw SyntaxError("Sorry this is not allowed")
+}
 
 let sum = parseInt(a) + parseInt(b)
 
-console.log(sum);
+try {
+    console.log(sum*x);
+} catch (error) {
+    console.log("this is unexpected");
+    
+}
