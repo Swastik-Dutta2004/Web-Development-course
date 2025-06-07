f99843103f831d2bde6e1596a0dde7450ec51784
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-06-07 20:13:51 +0530

    2nd commit

--- JS/VIDEO_80/script.js
+++ JS/VIDEO_80/script.js
@@ -1,10 +1,27 @@
-let animal = {
-    eats: true
-};
+// let animal = {
+//     eats: true
+// };
 
-let rabbit = {
-    jumps: true
-};
+// let rabbit = {
+//     jumps: true
+// };
 
-Object.setPrototypeOf(rabbit, animal);
+// Object.setPrototypeOf(rabbit, animal);
 
+class animal {
+    constructor(name,place) {
+        this.name = name
+        this.place =  place
+    }
+
+    eat(){
+        console.log("I am eating");
+    }
+
+    going(){
+        console.log("I am going");
+        
+    }
+}
+
+let a = new animal("SWasTiK","kirnahar")
