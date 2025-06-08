250ea53f624ba15c50572d87ec45a7593e0cddcc
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-06-08 11:17:14 +0530

    5th commit

--- PRATICESET/CHAPTER_9/script.js
+++ PRATICESET/CHAPTER_9/script.js
@@ -1,21 +1,40 @@
- function getData(src) {
-  return new Promise((resolve, reject) => {
-    let script = document.createElement("script");
-    script.src =  src;
-    script.onload = () => resolve(src);
-    script.onerror = () => reject("Failed to load the script ❌❌")
-    document.head.appendChild(script);
-})
+//  function getData(src) {
+//   return new Promise((resolve, reject) => {
+//     let script = document.createElement("script");
+//     script.src =  src;
+//     script.onload = () => resolve(src);
+//     script.onerror = () => reject("Failed to load the script ❌❌")
+//     document.head.appendChild(script);
+// })
+// }
+
+// async function main() {    
+//     try{
+//         await getData('test.js')
+//         alert("File is not running")
+//     }
+//     catch (error){
+//         console.log(error);
+//         alert("Something went wrong")
+//     }
+// }
+// main()
+
+//Q.3>
+function getData() {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            reject("❌❌❌")
+        }, 3000);
+    })
 }
 
-async function main() {    
-    try{
-        await getData('test.js')
-        alert("File is not running")
-    }
-    catch (error){
+async function reject() {
+    try {
+        let rej = await getData()
+    } catch (error) {
         console.log(error);
-        alert("Something went wrong")
+        
     }
 }
-main()
+reject()
