32bf8a1f0d269aca12a8ebeaf07ddf9d07a9316d
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-06-08 08:53:08 +0530

    3rd commit

--- JS/VIDEO_82/script.js
+++ JS/VIDEO_82/script.js
@@ -7,8 +7,22 @@ async function name() {
 }
 
 (async function main() {
-    let a = await name()
-    console.log(a);
-    let b = await name()
-    console.log(b);
+    // let a = await name()
+    // console.log(a);
+    // let b = await name()
+    // console.log(b);
+    
+    // let [x,y,...rest] = [5,6,7,42];
+    // console.log(x,y,rest);
+    
+    let obj = {
+        a:1,
+        b:5,
+        c:6,
+        d:8
+    }
+
+    let {a,b} = obj
+    console.log(a,b);
+    
 })()
