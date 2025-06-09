f8271624f35199e6877b0e2ffa5e7a3c09be3047
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-06-09 21:31:55 +0530

    5th commit

--- PRATICESET/CHAPTER_8/script.js
+++ PRATICESET/CHAPTER_8/script.js
@@ -6,4 +6,15 @@ function addBookMark(){
         console.log("Enter the name of the site and its url");
     }
 }
-let list = document.getElementById('')
+let list = document.getElementById('bookmarksList')
+let a = document.createElement("a")
+a.href = url
+a.textContent = name
+a.target = "_blank"
+
+let div =  document.createElement("div")
+div.appendChild(a)
+list.appendChild(div)
+
+document.getElementById("sitename").value
+document.getElementById("siteUrl").value
