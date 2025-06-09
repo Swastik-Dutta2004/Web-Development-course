0fa67b66f858bf1f04898647387bf5ec63c36108
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-06-09 20:03:59 +0530

    2nd commit

--- PRATICESET/CHAPTER_11/script.js
+++ PRATICESET/CHAPTER_11/script.js
@@ -1,23 +1,45 @@
 // Q1,2
-class complexNumber {
-    constructor(real,complex){
-        this.real = real
-        this.complex = complex
-    }
+// class complexNumber {
+//     constructor(real,complex){
+//         this.real = real
+//         this.complex = complex
+//     }
 
-    display(){
-        console.log(`${this.real} + ${this.complex}`);
+//     display(){
+//         console.log(`${this.real} + ${this.complex}`);
         
+//     }
+//     add(other){
+//         let newReal = this.real + other.real
+//         let newComplex = this.complex + other.complex
+//         return new complexNumber(newReal,newComplex)
+//     }
+// }
+
+// let a = new complexNumber(5,8)
+// let b = new complexNumber(2,7)
+
+// let result = a.add(b)
+// result.display()
+
+//Q3
+
+class human {
+    eat(){
+        console.log("Human love to eat");    
     }
-    add(other){
-        let newReal = this.real + other.real
-        let newComplex = this.complex + other.complex
-        return new complexNumber(newReal,newComplex)
+    peace(){
+        console.log("peace is most important thing in human life");
     }
 }
 
-let a = new complexNumber(5,8)
-let b = new complexNumber(2,7)
+class student extends human{
+    peace(){
+        console.log("There are some of the student who are traumatized for the pressure in their study life");
+        
+    }
+}
 
-let result = a.add(b)
-result.display()
+let s = new student()
+s.eat()
+s.peace()
