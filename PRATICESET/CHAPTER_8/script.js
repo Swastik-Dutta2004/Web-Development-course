d8fc217d8c56f42fb41f00cea9b61b49d681d2e3
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-06-09 22:07:24 +0530

    6th commit

--- PRATICESET/CHAPTER_8/script.js
+++ PRATICESET/CHAPTER_8/script.js
@@ -1,20 +1,21 @@
 function addBookMark(){
-    let name = document.querySelector("sitename").value
-    let url = document.querySelector("siteUrl").value
+    let name = document.querySelector("#sitename").value
+    let url = document.querySelector("#siteUrl").value
 
     if(!name || !url){
         console.log("Enter the name of the site and its url");
     }
+    
+    let list = document.getElementById('bookmarksList')
+    let a = document.createElement("a")
+    a.href = url
+    a.textContent = name
+    a.target = "_blank"
+    
+    let div =  document.createElement("div")
+    div.appendChild(a)
+    list.appendChild(div)
+    
+    document.getElementById("sitename").value = ""
+    document.getElementById("siteUrl").value = ""
 }
-let list = document.getElementById('bookmarksList')
-let a = document.createElement("a")
-a.href = url
-a.textContent = name
-a.target = "_blank"
-
-let div =  document.createElement("div")
-div.appendChild(a)
-list.appendChild(div)
-
-document.getElementById("sitename").value
-document.getElementById("siteUrl").value
