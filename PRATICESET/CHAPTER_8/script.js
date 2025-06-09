f8097142e5ad8951b3792fd672e9767eadfd0f61
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-06-09 21:19:51 +0530

    4th commit

---
+++ PRATICESET/CHAPTER_8/script.js
@@ -0,0 +1,9 @@
+function addBookMark(){
+    let name = document.querySelector("sitename").value
+    let url = document.querySelector("siteUrl").value
+
+    if(!name || !url){
+        console.log("Enter the name of the site and its url");
+    }
+}
+let list = document.getElementById('')
