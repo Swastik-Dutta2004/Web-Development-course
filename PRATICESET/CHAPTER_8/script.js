bf17534328d9a820f206ea23a2551051907c833b
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-06-10 08:06:19 +0530

    2nd commit

--- PRATICESET/CHAPTER_8/script.js
+++ PRATICESET/CHAPTER_8/script.js
@@ -1,4 +1,29 @@
-function addBookMark(){
+// Q2
+// function addBookMark(){
+//     let name = document.querySelector("#sitename").value
+//     let url = document.querySelector("#siteUrl").value
+
+//     if(!name || !url){
+//         console.log("Enter the name of the site and its url");
+//     }
+    
+//     let list = document.getElementById('bookmarksList')
+//     let a = document.createElement("a")
+//     a.href = url
+//     a.textContent = name
+//     a.target = "_blank"
+    
+//     let div =  document.createElement("div")
+//     div.appendChild(a)
+//     list.appendChild(div)
+    
+//     document.getElementById("sitename").value = ""
+//     document.getElementById("siteUrl").value = ""
+// }
+
+//Q3
+
+document.getElementById("addBookMark").addEventListener("click", function(){
     let name = document.querySelector("#sitename").value
     let url = document.querySelector("#siteUrl").value
 
@@ -18,4 +43,5 @@ function addBookMark(){
     
     document.getElementById("sitename").value = ""
     document.getElementById("siteUrl").value = ""
-}
+
+    })
