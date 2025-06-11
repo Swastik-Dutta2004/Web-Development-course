4d3019c8a70a98da33b088a99be2a73e1e2c5051
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-06-11 10:07:58 +0530

    7th commit

--- PRATICESET/GITHUB/5th_pro.js
+++ PRATICESET/GITHUB/5th_pro.js
@@ -1,16 +1,14 @@
-let arr = [0,-1,-2,-3,-4]
+let arr = [5, 10, 3, 7, -2, 6, 8]
 
 function sum(){
     let a = 0
-    for (let i = 0; i < arr.length; i++) {
-        let b = arr[i];
-            a = a + b
-
-            if(a < 0){
-                console.log("its a negative number");
-                break
-            }
-    }    
+    for(let i = 0; i < arr.length; i++){
+       if(arr[i] < 0 ){
+        console.log("it is a negetive number "+ arr[i]);
+        break;
+       }
+       a += arr[i]
+    }
     console.log(a);
     
 }
