e511673613134886ba90b8175c405dcfcd2c7df3
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-06-11 08:31:28 +0530

    2nd commit

--- PRATICESET/GITHUB/3rd_pro.js
+++ PRATICESET/GITHUB/3rd_pro.js
@@ -4,5 +4,10 @@ for (let i = 0; i  < arr.length; i++) {
     const element = arr[i];
     console.log(element);  
 }
-let b = arr.reverse()
-console.log(b);
+// let b = arr.reverse()
+// console.log(b);
+for (let j = arr.length - 1; j >= 0; j--){
+    const b = arr[j]
+    console.log(b);
+    
+}
