43737ca025e6c7363b7487c08c3802ec7c2c52c3
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-06-15 08:40:56 +0530

    2nd commit

---
+++ PRATICESET/GITHUB/13_pro.js
@@ -0,0 +1,8 @@
+let product = [
+    {name: "shope", quantities: "5ps", price: "30Rs" },
+    {name: "shampoo", quantities: "1L", price: "450Rs" },
+    {name: "biscute", quantities: "1pa", price: "30Rs" },
+    {name: "", quantities: "5ps", price: "30Rs" },
+    {name: "shope", quantities: "5ps", price: "30Rs" }
+    
+]
