c53a3981b24bf12707018aaa33ce7bbba16017a1
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-06-15 09:10:56 +0530

    3rd commit

--- PRATICESET/GITHUB/13_pro.js
+++ PRATICESET/GITHUB/13_pro.js
@@ -1,8 +1,22 @@
 let product = [
-    {name: "shope", quantities: "5ps", price: "30Rs" },
-    {name: "shampoo", quantities: "1L", price: "450Rs" },
-    {name: "biscute", quantities: "1pa", price: "30Rs" },
-    {name: "", quantities: "5ps", price: "30Rs" },
-    {name: "shope", quantities: "5ps", price: "30Rs" }
+    {name: "shope", quantities: "5ps", price: "30" },
+    {name: "shampoo", quantities: "1L", price: "450" },
+    {name: "biscute", quantities: "1pa", price: "30" },
+    {name: "oil", quantities: "1L", price: "300" },
+    {name: "Rice", quantities: "1kg", price: "45" }
     
 ]
+
+function calculateTotal (product){
+    let total = 0 ;
+    
+    product.forEach(item => {
+        let quantitynumber =  parseFloat(item.quantities)
+        let prices = parseFloat(item.price)
+
+        total += quantitynumber*prices
+    });
+    return total
+}
+let totalCost = calculateTotal(product)
+console.log("Total Cost:",totalCost);
