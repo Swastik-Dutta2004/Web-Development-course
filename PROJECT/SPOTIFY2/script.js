8acbfeb4288b59ac9055faecb7695203a0795d8a
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-07-05 10:20:57 +0530

    6th commit

--- PROJECT/SPOTIFY2/script.js
+++ PROJECT/SPOTIFY2/script.js
@@ -1,23 +1,37 @@
 async function getSongs() {
-    let a = await fetch("http://127.0.0.1:3000/PROJECT/SPOTIFY2/songs/")
-    let result = await a.text();
-    console.log(result);
-    let div = document.createElement("div")
-    div.innerHTML = result
-    let as = div.getElementsByTagName("a")
-    
-    let songs = []
-    for (let index = 0; index < as.length; index++) {
-        const element = as[index];
-        if(element.href.endsWith(".mp3")){
-            songs.push(element.href)
-        }
+  let a = await fetch("http://127.0.0.1:3000/PROJECT/SPOTIFY2/songs/");
+  let result = await a.text();
+  console.log(result);
+  let div = document.createElement("div");
+  div.innerHTML = result;
+  let as = div.getElementsByTagName("a");
+
+  let songs = [];
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href.split("/songs/")[1]);
     }
-    return songs
-    
+  }
+  return songs;
 }
 async function main() {
-    let songs = await getSongs()
-    console.log(songs);
+  //get the list of all the songs
+  let songs = await getSongs();
+  console.log(songs);
+
+  let songul = document.querySelector(".song-list").getElementsByTagName("ul")[0]
+  for (const song of songs) {
+    songul.innerHTML = songul.innerHTML + `<li> ${song.replaceAll("%20"," ")} </li>`
+    
+  }
+
+  var audio = new Audio(songs[0]);
+  audio.play();
+
+  audio.addEventListener("loadeddata", () => {
+    console.log(audio.duration, audio.currentSrc, audio.currentTime);
+    // The duration variable now holds the duration (in seconds) of the audio clip
+  });
 }
-main()
+main();
