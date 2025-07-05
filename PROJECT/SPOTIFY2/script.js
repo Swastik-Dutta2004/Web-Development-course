3338e367940bb04e22cd11605495e0c90813dd48
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-07-05 08:33:50 +0530

    4th commit

---
+++ PROJECT/SPOTIFY2/script.js
@@ -0,0 +1,23 @@
+async function getSongs() {
+    let a = await fetch("http://127.0.0.1:3000/PROJECT/SPOTIFY2/songs/")
+    let result = await a.text();
+    console.log(result);
+    let div = document.createElement("div")
+    div.innerHTML = result
+    let as = div.getElementsByTagName("a")
+    
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if(element.href.endsWith(".mp3")){
+            songs.push(element.href)
+        }
+    }
+    return songs
+    
+}
+async function main() {
+    let songs = await getSongs()
+    console.log(songs);
+}
+main()
