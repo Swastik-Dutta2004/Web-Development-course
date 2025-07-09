5882758ad4223acc13028d3bd9befd472cdfbef2
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-07-09 13:30:08 +0530

    6th commit

---
+++ VIDEO_87/aboutpath.js
@@ -0,0 +1,9 @@
+import path from "path"
+
+let mypath = "c:\\Users\\dutta\\OneDrive\\Desktop\\DO OR DIE\\Web-Development-course\\VIDEO_87\\swastik.txt"
+
+console.log(path.extname(mypath));
+
+console.log(path.dirname(mypath));
+console.log(path.basename(mypath));
+
