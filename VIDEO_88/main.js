f3a878922730bb65da6b2a25872766b5d648a16c
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-07-10 09:45:47 +0530

    today 1st commit

--- VIDEO_88/main.js
+++ VIDEO_88/main.js
@@ -5,6 +5,15 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World2!')
 })
+app.get('/about/:slug/', (req, res) => {
+  res.send(`About us ${req.params.slug}`)
+})
+// app.get('/contact', (req, res) => {
+//   res.send('contact us')
+// })
+// app.get('/blog', (req, res) => {
+//   res.send('blog')
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ VIDEO_89/main.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const { log } = require('util')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    console.log("hey it a get request");
+  res.send('Hello World get!')
+})
+
+app.post('/', (req, res) => {
+    console.log("hey it a post request");
+  res.send('Hello World post!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
