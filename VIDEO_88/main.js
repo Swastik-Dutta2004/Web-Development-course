1cb2cb34998658fa2bf5ca1ccb1b1513558693f2
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-07-12 18:55:37 +0530

    10th commit

--- VIDEO_88/main.js
+++ VIDEO_88/main.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+
 app.get('/:slug', (req, res) => {
   console.log(req.params);
   console.log(req.query);
