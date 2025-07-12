4ba9858ad02bf0924ac5af5a6137b9ba7ddf8a41
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-07-12 13:38:20 +0530

    today 1st commit

--- VIDEO_89/main.js
+++ VIDEO_89/main.js
@@ -15,6 +15,24 @@ app.post('/', (req, res) => {
   res.send('Hello World post!')
 })
 
+app.put('/', (req, res) => {
+    console.log("hey it a put request");
+  res.send('Hello World put!')
+})
+
+app.delete('/', (req, res) => {
+    console.log("hey it a delete request");
+  res.send('Hello World delete !')
+})
+app.get("/index", (req, res) => {
+    console.log("hey it a index request");
+  res.sendFile('template/index.html',{root:__dirname})
+})
+
+app.get('/api', (req, res) => {
+  res.json({a:1,b:2,c:3,d:4,name: ["swa","dut"]})
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
