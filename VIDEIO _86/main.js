59ae8e4c0df63a245b318c23784014bf48393a0d
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-07-12 13:50:28 +0530

    2nd commit

--- VIDEIO _86/main.js
+++ VIDEIO _86/main.js
@@ -1,15 +1,6 @@
-// const http = require('node:http')
-// const fs = require("fs")
-import http from "http"
+import {a} from "./mymodule.js"
+console.log(a);
 
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>hello world</h1>');
-});
+import swa from "./mymodule.js"
+console.log(swa);
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
---
+++ VIDEIO _86/mymodule.js
@@ -0,0 +1,11 @@
+export const a = 5
+export const b = 7
+export const c = 7
+export const d = 7
+export const e = 7
+
+let obj
+export default obj = {
+    x:5,
+    y:6
+}
