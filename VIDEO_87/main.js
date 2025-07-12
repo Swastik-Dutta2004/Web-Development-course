c809c30515fb3c94e3c7589fe42c67063a5077b2
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-07-12 16:12:01 +0530

    4th commit

--- VIDEO_87/main.js
+++ VIDEO_87/main.js
@@ -1,18 +1,17 @@
-
 const { error } = require("console");
-const fs = require("fs");
+const fs = require("fs")
+
 console.log("starting");
 
-fs.writeFile("swastik.txt", "swa is a good boy",() => {
-    console.log("done writing");
-    fs.readFile("swastik.txt",(error,data) => {
+fs.writeFile("swa.txt","the goat", ()=>{
+    console.log("done");
+    fs.readFile("swa.txt",(error,data) =>{
         console.log(error,data.toString());
     })
-    fs.appendFile("swastik.txt", "swastikdutttasckehkolsoi", (e, d) => {
-        console.log(d);
-        
-    })
-});
-
+})
 
 console.log("ending");
+
+
+
+
--- VIDEO_87/mainPromise.js
+++
@@ -1,7 +0,0 @@
-import { writeFile } from "fs";
-import fs from "fs/promises"
-
-let a =  await fs.readFile("swastik.txt")
-
-let b = await fs.appendFile("swastik.txt","\n\n\n\n\n dbcisxlsx")
-console.log(a.toString(),b);
