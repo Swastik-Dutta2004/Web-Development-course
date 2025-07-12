df703e207b5c558c17ed8db2c6f3f39d00336945
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-07-12 18:14:29 +0530

    8th commit

--- VIDEO_88/main.js
+++ VIDEO_88/main.js
@@ -5,12 +5,12 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World23!')
 })
-app.get('/about', (req, res) => {
-  res.send('Hello about!')
-})
-app.get('/contact', (req, res) => {
-  res.send('contact us')
-})
+// app.get('/about', (req, res) => {
+//   res.send('Hello about!')
+// })
+// app.get('/contact', (req, res) => {
+//   res.send('contact us')
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
