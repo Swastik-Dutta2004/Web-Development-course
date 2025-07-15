fccd9f995d486b6e18a05dbcfed8bf47d01948a9
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-07-15 20:46:17 +0530

    7th commit

---
+++ VIDEO_93/index.js
@@ -0,0 +1,5 @@
+import fs from "fs/promises"
+import fsn from "fs"
+
+let files = await fs.readdir("Users\\dutta\\OneDrive\\Desktop\\DO OR DIE\\Web-Development-course\\VIDEO_93");
+console.log(file);
