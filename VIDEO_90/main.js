822b00fc087c8692ee5f9cc9336a6814baddd9ee
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-07-15 13:17:07 +0530

    2nd commit

--- VIDEO_90/main.js
+++ VIDEO_90/main.js
@@ -2,7 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static("public"))
+// app.use(express.static("public"))
+
+app.use( (req, res, next)=> {
+  console.log('M1')
+  next()
+})
+
+app.use( (req, res, next)=> {
+  console.log('M2')
+  next()
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
