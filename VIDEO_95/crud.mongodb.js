386f083a1ddde7e0834071db6088bb4277c4cf14
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-07-17 12:02:43 +0530

    3rd commit

---
+++ VIDEO_95/crud.mongodb.js
@@ -0,0 +1,10 @@
+use("curddb")
+db.createCollection("courses")
+
+db.courses.insertOne({
+    name:"swa learning web dev but not have so much time",
+    price:1000000,
+    assignment:5000,
+    poject:10000
+})
+
