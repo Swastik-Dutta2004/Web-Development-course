40633f0fbacec821188b57060330a3bdc046a5dd
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-07-20 17:11:03 +0530

    4th commit

--- VIDEO_96/main.js
+++ VIDEO_96/main.js
@@ -1,24 +1,25 @@
 import mongoose from "mongoose";
-import express from "express"
-import { todo } from "./models/todo.js";
+import express from "express";
+import { todo as Todo } from "./models/todo.js";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-(async ()=>{
-   
-        await mongoose.connect("mongodb://localhost:27017/todo")
+(async () => {
+  await mongoose.connect("mongodb://localhost:27017/todo");
+  console.log("MongoDB connected");
 
-        app.get('/', (req, res) => {
-            const todo = new todo({title: "hey First todo", desc:"Description of the todo", isDone: false
-            });
-            await todo.save()
-          res.send('Hello World!')
-        })
-        
-        app.listen(port, () => {
-          console.log(`Example app listening on port ${port}`)
-        })
-    }
+  app.get('/', async (req, res) => {
+    const myTodo = new Todo({
+      title: "hey First todo",
+      desc: "Description of the todo",
+      isDone: false
+    });
+    await myTodo.save();
+    res.send('Todo saved and Hello World!');
+  });
 
-)
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+})();
