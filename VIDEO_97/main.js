1f0174e4eba9ed168f33f519d76d103a9bedb8b5
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-07-25 09:10:48 +0530

    5th commit

--- VIDEO_97/main.js
+++ VIDEO_97/main.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
+await mongoose.connect('mongodb://127.0.0.1:27017/conmpany');
 const port = 3000
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
+    res.render('index');})
+
+app.get('/Generator', (req, res) => {
     res.render('index');})
 
 app.listen(port, () => {
