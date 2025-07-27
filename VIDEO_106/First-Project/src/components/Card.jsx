1c92606fed88c5edc57b5d23559eccdf36316665
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-07-27 08:07:25 +0530

    today 1st commit

---
+++ VIDEO_106/First-Project/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Card.css"
+
+const Card = (props) => {
+  return (
+    <div>
+      <div className="card" style={{overflow:"hidden"}}>
+        <img src="https://cdn.shopify.com/s/files/1/0306/6419/6141/articles/coding_languages.png?v=1619126283" alt="" width={333} style={{border:"2px solid black"}}/>
+        <h1>{props.title}</h1>
+        <p>{props.description}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
---
+++ VIDEO_108/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = (color) => {
+  return (
+    <div>
+      Hey i am {color} color NAvbar 
+      hehe tumara maikichodo 
+    </div>
+  )
+}
+
+export default Navbar
