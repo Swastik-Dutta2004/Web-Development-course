b903a19800dcc73eeceb0f980657e5790cba825c
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-07-27 12:54:35 +0530

    8th commit

--- VIDEO_110/src/App.jsx
+++ VIDEO_110/src/App.jsx
@@ -6,6 +6,25 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   const [boobs, setboobs] = useState(true)
+  const [todo, settodo] = useState([
+    {
+      title:"Hey",
+      desc:"stays productive "
+    },
+    {
+      title:"yoo bro",
+      desc:"love to watch movies"
+    },
+    {
+      title:"How are you ",
+      desc:"Obident"
+    }
+  ])
+
+  const Todo = (todo) =>{return(<>
+  <div className="todo">todo.title</div>
+  <div className="todo">todo.desc</div>
+  </>)}
 
   return (
     <>
@@ -18,12 +37,13 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      {/* {boobs?<button>I can relased you stress 🙌🍑🍑💦💦</button>:"laund"} */}
+      {boobs?<button>I can relased you stress 🙌🍑🍑💦💦</button>:<button>Laund</button>}
 
-      {boobs&&<button>I can relased you stress 🙌🍑🍑💦💦</button>}
+      <Todo/>
+      {/* {boobs&&<button>I can relased you stress 🙌🍑🍑💦💦</button>} */}
       <div className="card">
         <button onClick={() => setboobs(!boobs)}>
-          count is {count}
+          Toggle me Honey
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
