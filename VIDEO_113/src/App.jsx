dd291d258bd9321b3d00798a701d21d25352614b
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-07-28 20:11:05 +0530

    9th commit

--- VIDEO_113/src/App.jsx
+++ VIDEO_113/src/App.jsx
@@ -23,7 +23,23 @@ function App() {
   return (
     <>
       <Navbar />
-     
+      <div className="container mx-auto my-5 rounded-xl p-5 bg-orange-300 min-h-[80vh]">
+        <div className="addtodo flex flex-col items-center justify-center gap-4">
+          <h2 className="text-lg font-bold">Add Todo</h2>
+          <input
+            className="text-black w-80 p-2 rounded"
+            value={todo}
+            type="text"
+            onChange={HandelChange}
+          />
+          <button
+            className="bg-orange-600 hover:bg-orange-700 px-4 py-2 rounded-xl font-bold text-white"
+            onClick={HandelAdd}
+          >
+            Add
+          </button>
+        </div>
+
         <h2 className="text-lg font-bold p-2">Your todo list</h2>
 
         <div className="todos">
@@ -40,7 +56,7 @@ function App() {
                 </div>
 
                 <div className="buttons">
-                
+                  
                 </div>
               </div>
             );
