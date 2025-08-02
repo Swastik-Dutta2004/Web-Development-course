febc6513cd828a77b7621a67e9f52e52fa2ea8e4
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-08-02 19:27:56 +0530

    2nd commit

--- VIDEO_117/src/App.jsx
+++ VIDEO_117/src/App.jsx
@@ -3,12 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
+const num = new Array (30_000_000).fill(0).map((_, i) => {
+  return{
+    index: i,
+    ismagical: i===29_000_000
+  }
+})
 
 function App() {
   const [count, setCount] = useState(0)
   const [numbers, setnumbers] = useState(num)
 
+  const magical = numbers.find(item => item.ismagical === true)
 
   return (
     <>
