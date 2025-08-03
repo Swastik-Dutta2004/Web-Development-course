e947f651721e06ea6096ae5be128f28e9dac11dc
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-08-03 13:51:44 +0530

    9th commit

--- VIDEO_119/src/App.jsx
+++ VIDEO_119/src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import { useForm } from "react-hook-form"
-import './App.css'
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import "./App.css";
 
 function App() {
   const {
@@ -10,38 +10,58 @@ function App() {
     formState: { errors, isSubmitting },
   } = useForm();
 
-  const delay =  (d) => {
-    return new Promise((resolve, reject)=>{
+  const delay = (d) => {
+    return new Promise((resolve, reject) => {
       setTimeout(() => {
-        resolve()
-      }, d* 1000);
-    })
-  }
+        resolve();
+      }, d * 1000);
+    });
+  };
 
   const onSubmit = async (data) => {
-    await delay(2)
-    console.log(data)
-  }
-  
+    await delay(2);
+    console.log(data);
+  };
+
   return (
     <>
-    {isSubmitting && <div>Loading...</div>}
-     <div className="conatiner">
-      <form action="" onSubmit={handleSubmit(onSubmit)}>
-        <div className="username">
-        <input  {...register("username", {required: {value:true, message:"These filed is required"}, minLength: {value:3, message:"Min length is 3"}, maxLength: {value:8, message:"Max length is 8"}})} type="text" placeholder='Username'/>
-        {errors.username && <div className='red'>{errors.username.message}</div>}
-        </div>
+      {isSubmitting && <div>Loading...</div>}
+      <div className="conatiner">
+        <form action="" onSubmit={handleSubmit(onSubmit)}>
+          <div className="username">
+            <input
+              {...register("username", {
+                required: { value: true, message: "These filed is required" },
+                minLength: { value: 3, message: "Min length is 3" },
+                maxLength: { value: 8, message: "Max length is 8" },
+              })}
+              type="text"
+              placeholder="Username"
+            />
+            {errors.username && (
+              <div className="red">{errors.username.message}</div>
+            )}
+          </div>
 
-        <div className="password">
-        <input {...register("password")} type="password" placeholder='Password'/>
-        </div>
+          <div className="password">
+            <input
+              {...register("password", {
+                required: { value: true, message: "These filed is required" },
+                minLength: { value: 3, message: "Min length is 3" },
+              })}
+              type="password"
+              placeholder="Password"
+            />
+            {errors.password && (<div className="red">{errors.password.message}</div>)}
+          </div>
 
-        <input disabled={isSubmitting} type="submit" value="submit" />
-      </form>
-     </div>
+          <div className="submit">
+            <input disabled={isSubmitting} type="submit" value="submit" />
+          </div>
+        </form>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
