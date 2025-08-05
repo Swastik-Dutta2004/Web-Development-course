2b7aac4d8e354a3d3f82976f399f46e54d8e696e
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-08-05 21:30:09 +0530

    13th commit

--- VIDEO_120/src/App.jsx
+++ VIDEO_120/src/App.jsx
@@ -3,15 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { useSelector, useDispatch } from 'react-redux'
+import { increment,decrement,multiply } from './redux/counter/counterSlice'
 
 
 function App() {
   const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
 
   return (
    <>
-    <div>
-      {count}
+    <div className='buttons'>
+      <button>-</button>
+      currenty the value is {count}
+      <button onClick={() => {dispatch(increment())}}>+</button>
     </div>
    </>
   )
