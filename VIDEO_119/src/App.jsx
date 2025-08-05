5e184eea605cc838b49668dddce22e6ebe89d529
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-08-05 17:42:57 +0530

    5th commit

--- VIDEO_119/src/App.jsx
+++ VIDEO_119/src/App.jsx
@@ -5,24 +5,26 @@ import "./App.css";
 
 
 function App() {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+  
 
 
   return (
     <>
     <div className="container">
       <form onSubmit={handleSubmit(onSubmit)}>
+
         <div className="useername">
-          <input {...register("username")} type="text" name="userename" />
+          <input {...register("username",{ required: {value:true, message:"This field is required"}, minLength:{value:3, message:"Atlest 3 chracter needed"}, maxLength:{value:8, message:"upto 8 chracters"} })} type="text"  placeholder="Username" />
+          {errors.username && <div className="red">{errors.username.message}</div>}
         </div>
 
         <div className="password">
-          <input {...register("password",)} type="password" name="password" id="" />
+          <input {...register("password",{ required: {value:true, message:"This field is required"}, minLength:{value:3, message:"Atlest 3 chracter needed"}, maxLength:{value:8, message:"upto 8 chracters"}})} type="password" id="" placeholder="Password"/>
+          {errors.password && <div className="red">{errors.password.message}</div>}
         </div>
 
         <div className="submit">
-          <input type="submit" name="submit" />
+          <input  type="submit" name="submit" />
         </div>
       </form>
     </div>
