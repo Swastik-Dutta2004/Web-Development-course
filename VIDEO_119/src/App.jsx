b752f4b6be6fbb044288c3926a7f8cdf272d7831
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-08-05 18:03:30 +0530

    7th commit

--- VIDEO_119/src/App.jsx
+++ VIDEO_119/src/App.jsx
@@ -5,10 +5,10 @@ import "./App.css";
 
 
 function App() {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const { register, handleSubmit, setError, formState: { errors, isSubmitting } } = useForm();
   
   const delay = (d)=> {
-    return new Promise((reject, resolve) => {
+    return new Promise(( resolve, reject) => {
       setTimeout(() => {
         resolve()
       }, d*1000);
@@ -18,26 +18,31 @@ function App() {
 const onSubmit =  async (data) => {
   await delay(2)
   console.log(data);
+  if(data.username !== "shubham"){
+    setError("myform",{message:"Your form is not in good order"})
+  }
 }
 
 
   return (
     <>
+    {isSubmitting && <div>...Loading</div>}
     <div className="container">
       <form onSubmit={handleSubmit(onSubmit)}>
 
         <div className="useername">
-          <input {...register("username",{ required: {value:true, message:"This field is required"}, minLength:{value:3, message:"Atlest 3 chracter needed"}, maxLength:{value:8, message:"upto 8 chracters"} })} type="text"  placeholder="Username" />
+          <input {...register("username",{ required: {value:true, message:"This field is required"}, minLength:{value:3, message:"Atlest 3 character needed"}, maxLength:{value:8, message:"upto 8 characters"} })} type="text"  placeholder="Username" />
           {errors.username && <div className="red">{errors.username.message}</div>}
         </div>
 
         <div className="password">
-          <input {...register("password",{ required: {value:true, message:"This field is required"}, minLength:{value:3, message:"Atlest 3 chracter needed"}, maxLength:{value:8, message:"upto 8 chracters"}})} type="password" id="" placeholder="Password"/>
+          <input {...register("password",{ required: {value:true, message:"This field is required"}, minLength:{value:3, message:"Atlest 3 character needed"}})} type="password" id="" placeholder="Password"/>
           {errors.password && <div className="red">{errors.password.message}</div>}
         </div>
 
         <div className="submit">
-          <input  type="submit" name="submit" />
+          <input disabled = {isSubmitting} type="submit" name="submit" />
+          {errors.myform && <div className="red">{errors.myform.message}</div>}
         </div>
       </form>
     </div>
