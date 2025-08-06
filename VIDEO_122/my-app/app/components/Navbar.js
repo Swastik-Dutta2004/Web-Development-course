93cd7c4b3b2de6488c988f4df8a719bc0a42a7ed
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-08-06 10:31:43 +0530

    6th commit

---
+++ VIDEO_122/my-app/app/components/Navbar.js
@@ -0,0 +1,10 @@
+"use client"
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>Hey i am, client side Navbar</div>
+  )
+}
+
+export default Navbar
--- VIDEO_122/my-app/app/page.js
+++ VIDEO_122/my-app/app/page.js
@@ -1,6 +1,7 @@
 // "use client"
 // import { useState, useEffect } from "react";
 import fs from "fs/promises"
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   // const [count, setcount] = useState(0)
@@ -11,6 +12,7 @@ export default function Home() {
   })
   return (
     <div>
+      <Navbar/>
       Hey I am a componets 
       {/* {count} */}
       {/* <button onClick={() => {setcount(count+1)}}>Jurk me</button> */}
