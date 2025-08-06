8958a8683560e85b42a67541bc83432f0f6c8e73
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-08-06 12:43:11 +0530

    7th commit

---
+++ VIDEO_123/my-app/app/contact/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>
+        hey i am contact
+    </div>
+  )
+}
+
+export default contact
+
+export const metadata = {
+  title: "Contact Facebook - connect with the world",
+  description: "This is contact of Facebook and we can connect with world with facebook",
+}
