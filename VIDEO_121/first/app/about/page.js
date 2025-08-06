8ff82349c6b17323025b78e340cdbdcce960a730
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-08-06 09:11:48 +0530

    3rd commit

--- VIDEO_121/first/app/about/page.js
+++ VIDEO_121/first/app/about/page.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>I am About</div>
+    <div>
+      <h1 className="text-xl">We are plaining to start a starup</h1>
+      <ul>
+        <li>There we have to work hard </li>
+        <li>It will be Online tickets generating app for the local bues</li>
+        <li>And  there will be a scaner for conductor also. </li>
+        <h1>SO stay Tune😁</h1>
+      </ul>
+    </div>
   )
 }
 
