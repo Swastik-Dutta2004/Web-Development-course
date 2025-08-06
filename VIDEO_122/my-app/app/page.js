2df8cb5b6ff628f798b5b56c5dadd9522fd27a9b
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-08-06 10:09:55 +0530

    5th commit

--- VIDEO_122/my-app/app/page.js
+++ VIDEO_122/my-app/app/page.js
@@ -1,12 +1,19 @@
-"use client"
-import { useState, useEffect } from "react";
+// "use client"
+// import { useState, useEffect } from "react";
+import fs from "fs/promises"
 
 export default function Home() {
-  const [count, setcount] = useState(0)
+  // const [count, setcount] = useState(0)
+  console.log("hey i am swa");
+  
+  let a = fs.readFile(".gitignore")
+  a.then(e => {console.log(e.toString());
+  })
   return (
     <div>
-      I am a componets {count}
-      <button onClick={() => {setcount(count+1)}}>Jurk me</button>
+      Hey I am a componets 
+      {/* {count} */}
+      {/* <button onClick={() => {setcount(count+1)}}>Jurk me</button> */}
     </div>
   );
 }
