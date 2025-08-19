9b1df3b9679814083ae014c84a44de5d8b4d0e53
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-08-19 12:42:11 +0530

    7th commit

--- VIDEO_125/server-actions/app/page.js
+++ VIDEO_125/server-actions/app/page.js
@@ -4,23 +4,22 @@ import fs from 'fs/promises'
 export default function Home() {
 const submitAction = async (e) => {
   "use server"
-  console.log(e.get("name"). e.get("Address"));
-  
-  let a = await fs.readFile("swa.txt","Hey its me SwAA 😁")
+  console.log(e.get("name"), e.get("Address"));
+  let a = await fs.writeFile("swa.txt","Hey its me SwAA 😁")
   console.log(a);
   
 }
 
   return (
     <div>
-      <form className="mx-auto my-5" action="{submitAction}">
+      <form className="mx-auto my-5" action={submitAction}>
         <div className="py-2">
           <label className="px-5" htmlFor="Name">Name</label>
-          <input className="bg-white text-black " type="text" name="name" id="" />
+          <input className="bg-white text-black " type="text" name="name" id="name" />
         </div>
         <div className=" py-2">
           <label className="px-5" htmlFor="Address ">Address</label>
-          <input className="bg-white text-black" type="text" name="Address" id="" />
+          <input className="bg-white text-black" type="text" name="Address" id="Address" />
         </div>
         <div className="py-2 px-5">
           <button className="bg-amber-500 border border-2-red">Click me</button>
