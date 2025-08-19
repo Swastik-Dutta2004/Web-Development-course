03c029c3712a1ea1c492f7db728b55c3353742ec
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-08-19 10:47:05 +0530

    4th commit

--- VIDEO_124/practics/app/page.js
+++ VIDEO_124/practics/app/page.js
@@ -1,22 +1,28 @@
+"use client"
 import Image from "next/image";
 
 export default function Home() {
   const handlechange = async () => {
-    let a = await fetch("./api/add",{method: "POST", 
-    headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({ username: "example1" }),
-  });
-  
-    let result = awaita.json()
+    let data = {
+      name: "Swastik",
+      roll: "coder"
+    }
+    let a = await fetch("./api/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    let result = await a.json()
     console.log(result);
-    
+
   }
   return (
     <div>
       <h1 className="text-xl font-bold text-teal-900 bg-amber-100">Next.js api routes</h1>
-      <button className="font-bold bg-amber-700 text-black" onClick={handlechange}>Click me</button>
+      <button className="font-bold bg-amber-700 text-black mx-auto" onClick={handlechange}>Click me</button>
     </div>
   );
 }
