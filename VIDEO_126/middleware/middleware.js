ba55cb8159da9a46e4ba5d3ca86952cc1ffe9e3f
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-08-20 21:19:37 +0530

    3rd commit

--- VIDEO_126/middleware/middleware.js
+++ VIDEO_126/middleware/middleware.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
-    return NextResponse.json({messsge: "Fuck off"})
+    // return NextResponse.json({messsge: "Fuck off"})
   return NextResponse.redirect(new URL('/', request.url))
 }
  
