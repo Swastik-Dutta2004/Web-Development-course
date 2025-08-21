99cbfcc960c90221560fa0d956b94cfdf2b87855
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-08-21 07:57:46 +0530

    today 1st commit

---
+++ VIDEO_126/middleware/app/home/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div>I am home page</div>
+  )
+}
+
+export default home
--- VIDEO_126/middleware/middleware.js
+++ VIDEO_126/middleware/middleware.js
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
     // return NextResponse.json({messsge: "Fuck off"})
-  return NextResponse.redirect(new URL('/', request.url))
+  return NextResponse.redirect(new URL('/home', request.url))
 }
  
 export const config = {
