667ff8219fd071e0cbf84e1f0692a42b764aa03f
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-09-01 20:08:36 +0530

    10th commit

--- VIDEO_130/passwordmanage/src/components/Navbar.jsx
+++ VIDEO_130/passwordmanage/src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='bg-purple-200 '>
+    <nav className='bg-purple-200 flex justify-between px-2 '>
         <div className='logo font-bold'>PAssManaGe</div>
-        <ul>
-            <li>
+        <ul className=''>
+            <li className='flex gap-3'>
                 <a href="">Home</a>
                 <a href="">Contact</a>
                 <a href="">About</a>
--- VIDEO_130/passwordmanage/tailwind.config.js
+++ VIDEO_130/passwordmanage/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",       // for Vite
+    "./src/**/*.{js,ts,jsx,tsx}", // for React files
   ],
   theme: {
     extend: {},
