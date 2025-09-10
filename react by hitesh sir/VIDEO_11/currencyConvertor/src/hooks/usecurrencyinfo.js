2b9b8abf070a42a0cc4f72c141f10405c249cd8f
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-09-10 19:24:50 +0530

    10th commit

--- react by hitesh sir/VIDEO_11/currencyConvertor/src/hooks/usecurrencyinfo.js
+++ react by hitesh sir/VIDEO_11/currencyConvertor/src/hooks/usecurrencyinfo.js
@@ -1,8 +1,13 @@
 import { useEffect, useState } from "react";
 
 function usecurrencyInfo (currency){
+    const [data, setdata] = useState({})
     useEffect(() => {
-    
-    }, [])
-    
+        fetch(` https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((res) => res.json())
+        .then((res) => setdata(res.currency))
+        console.log(data);
+    }, [currency])
+    console.log(data);
+    return data
 }
