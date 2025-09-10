1df3d819252abdcdeeb63bdac293367d3fa239fd
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-09-10 10:32:09 +0530

    6th commit

--- react by hitesh sir/VIDEO_10/password/src/App.jsx
+++ react by hitesh sir/VIDEO_10/password/src/App.jsx
@@ -31,12 +31,25 @@ function App() {
     <>
       <div className="w-full max-w-md mx-auto shadow-lg rounded-lg px-4 my-8 text-purple-800 bg-amber-500"> 
         <h1 className='text-2xl font-bold text-center'>PassWord Generator</h1>
-        <div className="flex">
+        <div className="flex gap-2">
           <input type="text"
             value={password}
             placeholder='Password'
-            className="w-full py-1 px-2 my-3 rounded bg-amber-300 text-black"
- />
+            className="w-full py-1 px-2 my-3 rounded bg-amber-300 text-black outline-none"
+            readOnly
+            />
+            <button className='bg-blue-600 text-lg font-bold rounded px-2 my-auto text-teal-50 p-1 hover:bg-blue-800 transition duration-150'>Copy</button>
+        </div>
+        <div className="flex gap-4">
+          <div className=" flex gap-x-2 items-center">
+            <input type="range"
+            max={100}
+            min={8}
+            value={length}
+            onChange={(e) => {setLength(e.target.value)}}
+            />
+            <label className='font-bold'>Lenght: {length}</label>
+          </div>
         </div>
       </div>
     </>
