ec2fe70f5c512f8aea5955d738413d410c0e226f
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-09-10 09:17:20 +0530

    4th commit

--- react by hitesh sir/VIDEO_10/password/src/App.jsx
+++ react by hitesh sir/VIDEO_10/password/src/App.jsx
@@ -1,18 +1,29 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-const [length, setLength] = useState(8)
-const [number, setNumber] = useState(false)
-const [char, setchar] = useState(false)
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setcharAllowed] = useState(false)
+  const [password, setpassword] = useState("")
 
-const [password, setpassword] = useState(second)
+  const passwordGenerator = useCallback(
+    () => {
+      let pass = ""
+      let string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+      if (numberAllowed) string += "0123456789"
+      if (charAllowed) string += "!@#$%^&*()_+-=[]{}|;:.<>?/`~"
+    }, [length, numberAllowed, charAllowed, setpassword],
+  )
+
+
   return (
-   <>
-   <h1 className='text-6xl text-center text-purple-600'>PAssword manager</h1>
-   </>
+    <>
+      <h1 className='text-6xl text-center text-purple-600'>PAssword manager</h1>
+    </>
   )
 }
 
