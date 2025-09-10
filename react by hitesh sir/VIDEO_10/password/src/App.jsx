f33dcc8b8bbeaaa5ffcb7ffc1c9af94d3fbe7324
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-09-10 08:51:31 +0530

    3rd commit

---
+++ react by hitesh sir/VIDEO_10/password/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+const [length, setLength] = useState(8)
+const [number, setNumber] = useState(false)
+const [char, setchar] = useState(false)
+
+const [password, setpassword] = useState(second)
+  return (
+   <>
+   <h1 className='text-6xl text-center text-purple-600'>PAssword manager</h1>
+   </>
+  )
+}
+
+export default App
