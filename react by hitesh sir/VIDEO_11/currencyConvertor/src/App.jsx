d13cd3876a12669ad918e2f5590061d281b4b3ce
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-09-11 10:29:43 +0530

    3rd commit

--- react by hitesh sir/VIDEO_11/currencyConvertor/src/App.jsx
+++ react by hitesh sir/VIDEO_11/currencyConvertor/src/App.jsx
@@ -7,8 +7,21 @@ function App() {
   const [amount, setamount] = useState(0)
   const [from, setfrom] = useState("usd")
   const [To, setTo] = useState("inr")
-  const [currencyConvertor, setCurrencyConvertor] = useState(0)
-  
+  const [CurrencyConvertor, setCurrencyConvertor] = useState(0)
+
+  const currencyInfo = usecurrencyInfo(from)
+  const option = Object.keys(currencyInfo)
+
+  const swap = () => {
+    setfrom(To)
+    setTo(from)
+    setamount(CurrencyConvertor)
+    setCurrencyConvertor(amount)
+
+    const convert = () => {
+      amount * currencyInfo(To)
+    }
+  }
   return (
     <>
       <h1 className='text-center text-4xl font-bold bg-amber-950 text-white'>Currency Convertor</h1>
