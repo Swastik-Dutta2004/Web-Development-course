11e91c87a0cf954b9638d4ad7f0e39109a471e74
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-09-14 22:45:27 +0530

    10th  commit

---
+++ react by hitesh sir/VIDEO_13/context_api/src/Context/UserContextProvider.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import useContext from "./UserContext";
+import { useState } from "react";
+
+const UserContextPorvider = ({ children }) => {
+    const [user, setuser] = useState(null)
+    return (
+        <useContext.Provider value={{user, setuser}}>
+            {children}
+        </useContext.Provider>
+    )
+}
