63eaf3e215e7447a2954cf78c64a91c7f6b21bbe
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-09-14 08:52:01 +0530

    5th commit

--- react by hitesh sir/VIDEO_12/reactRouter/src/main.jsx
+++ react by hitesh sir/VIDEO_12/reactRouter/src/main.jsx
@@ -3,16 +3,28 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+    {
+        path: "About",
+        element: <About />
+      }
+    ]
   }
 ])
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
