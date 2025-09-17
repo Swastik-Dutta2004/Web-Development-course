f9e3258183b076963fd866dc1323aa9eb84e06b0
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-09-17 08:55:42 +0530

    2nd commit

---
+++ react by hitesh sir/VIDEO_13/theme_Switcher/src/Context/Theme.js
@@ -0,0 +1,13 @@
+import { createContext, useContext } from "react";
+
+export const ThemeContext = createContext({
+    theme: "dark",
+    darkmode: () => {},
+    lightmode: () => {}
+})
+
+export const ThemeProvider = ThemeContext.Provider
+
+export const useTheme = () => {
+    
+}
