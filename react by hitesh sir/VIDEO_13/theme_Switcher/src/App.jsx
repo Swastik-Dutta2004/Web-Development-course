344645424cf6820293d5cdabc2855d361f378cf9
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-09-17 09:18:20 +0530

    4th commit

--- react by hitesh sir/VIDEO_13/theme_Switcher/src/App.jsx
+++ react by hitesh sir/VIDEO_13/theme_Switcher/src/App.jsx
@@ -1,23 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { ThemeProvider } from './Context/Theme'
 
 function App() {
+  const [themeMode, setthemeMode] = useState("light")
+
+  const darkMode = () => {
+    setthemeMode("dark")
+  }
+  
+  const lightMode = () => {
+    setthemeMode("light")
+  }
 
   return (
-    
-<div className="flex flex-wrap min-h-screen items-center">
-                <div className="w-full">
-                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        {/* ThemeBtn*/}
-                    </div>
+    <ThemeProvider value={{theme, darkMode, lightMode}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* ThemeBtn*/}
+          </div>
 
-                    <div className="w-full max-w-sm mx-auto">
-                       {/* card */}
-                    </div>
-                </div>
-            </div>
+          <div className="w-full max-w-sm mx-auto">
+            {/* card */}
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
 
   )
 }
--- react by hitesh sir/VIDEO_13/theme_Switcher/src/Context/Theme.js
+++ react by hitesh sir/VIDEO_13/theme_Switcher/src/Context/Theme.js
@@ -2,8 +2,8 @@ import { createContext, useContext } from "react";
 
 export const ThemeContext = createContext({
     theme: "dark",
-    darkmode: () => {},
-    lightmode: () => {}
+    darkmmde: () => {},
+    lightMode: () => {}
 })
 
 export const ThemeProvider = ThemeContext.Provider
