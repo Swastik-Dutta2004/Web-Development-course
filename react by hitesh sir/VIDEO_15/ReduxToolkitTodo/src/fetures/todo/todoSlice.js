2c58e2998cac2e5f523913b0fa6b1b62ee279f00
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-09-19 12:56:58 +0530

    3rd commit

--- react by hitesh sir/VIDEO_15/ReduxToolkitTodo/src/fetures/todo/todoSlice.js
+++ react by hitesh sir/VIDEO_15/ReduxToolkitTodo/src/fetures/todo/todoSlice.js
@@ -20,6 +20,13 @@ export const todoslice = createSlice({
         },
         RemovedTodo: (state, action) => {
             state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
+        upadateTodo: () => {
+            const {id, newText} = action.payload
+            const todo = state.todos.find((todo) => todo.id === id)
+            if(todo){
+                todo.text = newText
+            }
         }
     }
 })
