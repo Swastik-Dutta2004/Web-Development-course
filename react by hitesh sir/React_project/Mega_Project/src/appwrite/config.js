31f37f870f741cca932b4727258d318453b7ee52
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-10-11 20:37:19 +0530

    7th commit

---
+++ react by hitesh sir/React_project/Mega_Project/src/appwrite/config.js
@@ -0,0 +1,10 @@
+import conf from "../conf/conf";
+import { Client, Account, ID, Databases, Storage, Query} from 'appwrite';
+
+export class Service{
+    client = new Client()
+    Databases;
+    bucket;
+    constructor(){}
+}
+
