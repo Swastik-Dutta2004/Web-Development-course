1aa6896e28c12d81fb6e3f3d6c4e746126d61120
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-10-12 10:32:06 +0530

    4th commit

--- react by hitesh sir/React_project/Mega_Project/src/appwrite/config.js
+++ react by hitesh sir/React_project/Mega_Project/src/appwrite/config.js
@@ -1,20 +1,20 @@
 import conf from "../conf/conf";
-import { Client, Account, ID, Databases, Storage, Query} from 'appwrite';
+import { Client, Account, ID, Databases, Storage, Query } from 'appwrite';
 
-export class Service{
+export class Service {
     client = new Client()
     Databases;
     bucket;
 
-    constructor(){
+    constructor() {
         this.client
             .setEndpoint(conf.appwriteUrl)
             .setProject(conf.appwriteProjectId)
-        this.databases = new Databases(this.client) 
-        this.bucket = new Storage (this.client)
+        this.databases = new Databases(this.client)
+        this.bucket = new Storage(this.client)
     }
 
-    async createPost({title, slug, content, featuredImage, status, userID }){
+    async createPost({ title, slug, content, featuredImage, status, userID }) {
         try {
             return await this.databases.createDocument(
                 conf.appwriteDatabaseId,
@@ -26,11 +26,11 @@ export class Service{
             )
         } catch (error) {
             console.log("appwrite service :: createPost :: error", error);
-            
+
         }
     }
 
-    async updatePost({title, content, featuredImage, status}){
+    async updatePost(slug, { title, content, featuredImage, status }) {
         try {
             return await this.databases.updateDocument(
                 conf.appwriteDatabaseId,
@@ -42,7 +42,21 @@ export class Service{
             )
         } catch (error) {
             console.log("appwrite service :: updatePost :: error", error);
-            
+
+        }
+    }
+
+    async deletePost({slug }) {
+        try {
+             await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+            )
+            return true
+        } catch (error) {
+            console.log("appwrite service :: updatePost :: error", error);
+            return false
         }
     }
 }
