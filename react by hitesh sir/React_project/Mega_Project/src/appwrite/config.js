c07d503efb2e8a6e28477d8d1e7cb21215e12e9d
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-10-12 09:38:59 +0530

    3rd commit

--- react by hitesh sir/React_project/Mega_Project/src/appwrite/config.js
+++ react by hitesh sir/React_project/Mega_Project/src/appwrite/config.js
@@ -5,13 +5,45 @@ export class Service{
     client = new Client()
     Databases;
     bucket;
-    
+
     constructor(){
         this.client
             .setEndpoint(conf.appwriteUrl)
             .setProject(conf.appwriteProjectId)
         this.databases = new Databases(this.client) 
         this.bucket = new Storage (this.client)
+    }
+
+    async createPost({title, slug, content, featuredImage, status, userID }){
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title, content, featuredImage, status, userID
+                }
+            )
+        } catch (error) {
+            console.log("appwrite service :: createPost :: error", error);
+            
+        }
+    }
+
+    async updatePost({title, content, featuredImage, status}){
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title, content, featuredImage, status
+                }
+            )
+        } catch (error) {
+            console.log("appwrite service :: updatePost :: error", error);
+            
+        }
     }
 }
 
