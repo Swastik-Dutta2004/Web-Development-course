f4b760837bb9d4347ec8c7ef808087da3cc68179
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-10-13 20:28:54 +0530

    6th commit

--- react by hitesh sir/React_project/Mega_Project/src/Components/Header/Header.jsx
+++ react by hitesh sir/React_project/Mega_Project/src/Components/Header/Header.jsx
@@ -1,12 +1,43 @@
 import React from 'react'
 import { Container, Logo, LogoutBtn } from '../Index'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
+  const authService = useSelector((state) => state.auth.status)
+  const navigate =  useNavigate()
+
+
+  const navItem = [
+    {
+      name: 'Home',
+      slug:  "/",
+      active: true
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus
+    },
+    {
+      name: "Sigup",
+      slug: "/Sigup",
+      active: !authStatus
+    },
+    {
+      name: "All Posts",
+      slug: "/all-Post",
+      active: authStatus
+    },
+    {
+      name: "Add Post",
+      slug: "/add-Post",
+      active: authStatus
+    },
+  ]
   return (
-    <div>
-      header
-    </div>
+    <header></header>
   )
 }
 
