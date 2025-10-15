f39b524c852f8bdace68b06d58cfab98c05dea35
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-10-15 19:57:36 +0530

    4th commit

--- react by hitesh sir/React_project/Mega_Project/src/Components/RTE.jsx
+++ react by hitesh sir/React_project/Mega_Project/src/Components/RTE.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {Editor} from '@tinymce/tinymce-react'
 import { Controller } from 'react-hook-form'
 
-const RTE = ({name, control, label, deffaultvalue = ""}) => {
+const RTE = ({name, control, label, defaultValue = ""}) => {
   return (
     <div className='w-full'>
         {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
@@ -10,7 +10,7 @@ const RTE = ({name, control, label, deffaultvalue = ""}) => {
         <Controller
         name= {name || "context"}
         control={control}
-        render={({field: {onchange}}) => (
+        render={({field: {onChange}}) => (
              <Editor
         initialValue={defaultValue}
         init={{
