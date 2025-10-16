8963c3307f6ecde203be18b0456e1685240a113a
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-10-16 12:38:17 +0530

    5th commit

--- react by hitesh sir/React_project/Mega_Project/src/Components/PostForm/PostForm.jsx
+++ react by hitesh sir/React_project/Mega_Project/src/Components/PostForm/PostForm.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {useForm} from 'react-hook-form'
 import {Button, Input, Select, RTE} from '../Index'
 import appwriteService from '../../appwrite/config'
-import { useNavigate } from 'react-router-dom'
+import { data, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
 
@@ -17,9 +17,26 @@ function PostForm({Post}) {
   })
 
     const navigate = useNavigate()
-    const useData = useSelector(state => state.user.useData)
+    const userData = useSelector(state => state.user.useData)
 
-    
+    const submit = async (data) => {
+      if(Post){
+        const file = data.image[0] ? appwriteService.uplodeFile(data.image[0]) :null
+      }
+      if (file) {
+        appwriteService.deleteFile(Post.featureImage)
+      }
+
+       const dbPost = await appwriteService.updatePost(Post.$id,{
+        ...data,
+        featureImage: file ? file.$id : undefined,
+
+        if (dbPost) {
+          navigate(`/post/${dbPost.$id}`)
+        }
+       })
+
+    }
 
   return (
     <div>PostForm</div>
