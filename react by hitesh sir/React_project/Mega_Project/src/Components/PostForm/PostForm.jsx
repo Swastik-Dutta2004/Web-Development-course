27e2789c15c571917a4c36f9855fec3d41b5e5a0
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-10-16 17:58:48 +0530

    6th commit

--- react by hitesh sir/React_project/Mega_Project/src/Components/PostForm/PostForm.jsx
+++ react by hitesh sir/React_project/Mega_Project/src/Components/PostForm/PostForm.jsx
@@ -1,42 +1,57 @@
 import React from 'react'
-import {useForm} from 'react-hook-form'
-import {Button, Input, Select, RTE} from '../Index'
+import { useForm } from 'react-hook-form'
+import { Button, Input, Select, RTE } from '../Index'
 import appwriteService from '../../appwrite/config'
-import { data, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
-
-function PostForm({Post}) {
-  const {register, handleSubmit, watch, setValue, control, getValues} = useForm({
-      defaultValues: {
+function PostForm({ Post }) {
+  const { register, handleSubmit, control, setValue, getValues } = useForm({
+    defaultValues: {
       title: Post?.title || '',
       slug: Post?.slug || '',
-      contant: Post?.contant || '',
+      content: Post?.content || '',
       status: Post?.status || 'active',
-    }
+    },
   })
 
-    const navigate = useNavigate()
-    const userData = useSelector(state => state.user.useData)
+  const navigate = useNavigate()
+  const userData = useSelector((state) => state.user.userData)
 
-    const submit = async (data) => {
-      if(Post){
-        const file = data.image[0] ? appwriteService.uplodeFile(data.image[0]) :null
-      }
+  const submit = async (data) => {
+    if (Post) {
+      const file = data.image?.[0]
+        ? await appwriteService.uploadFile(data.image[0])
+        : null
+
       if (file) {
-        appwriteService.deleteFile(Post.featureImage)
+        await appwriteService.deleteFile(Post.featureImage)
       }
 
-       const dbPost = await appwriteService.updatePost(Post.$id,{
+      const dbPost = await appwriteService.updatePost(Post.$id, {
         ...data,
-        featureImage: file ? file.$id : undefined,
+        featureImage: file ? file.$id : Post.featureImage,
+      })
+
+      if (dbPost) {
+        navigate(`/post/${dbPost.$id}`)
+      }
+    } else {
+      const file = await appwriteService.uploadFile(data.image[0])
+
+      if (file) {
+        const dbPost = await appwriteService.createPost({
+          ...data,
+          userID: userData.$id,
+          featureImage: file.$id,
+        })
 
         if (dbPost) {
           navigate(`/post/${dbPost.$id}`)
         }
-       })
-
+      }
     }
+  }
 
   return (
     <div>PostForm</div>
