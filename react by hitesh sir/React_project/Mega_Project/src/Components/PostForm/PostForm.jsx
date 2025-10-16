20c35b6ce41edfd3f5df668a41c7b7e9020362e7
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-10-16 09:43:59 +0530

    4th commit

--- react by hitesh sir/React_project/Mega_Project/src/Components/PostForm/PostForm.jsx
+++ react by hitesh sir/React_project/Mega_Project/src/Components/PostForm/PostForm.jsx
@@ -1,6 +1,26 @@
-import React from 'react' 
+import React from 'react'
+import {useForm} from 'react-hook-form'
+import {Button, Input, Select, RTE} from '../Index'
+import appwriteService from '../../appwrite/config'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
-function PostForm() {
+
+function PostForm({Post}) {
+  const {register, handleSubmit, watch, setValue, control, getValues} = useForm({
+      defaultValues: {
+      title: Post?.title || '',
+      slug: Post?.slug || '',
+      contant: Post?.contant || '',
+      status: Post?.status || 'active',
+    }
+  })
+
+    const navigate = useNavigate()
+    const useData = useSelector(state => state.user.useData)
+
+    
+
   return (
     <div>PostForm</div>
   )
