2434cbd5a4b5cb07087aabf843e0faa8cb54ffb3
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-10-18 10:10:20 +0530

    4th commit

--- react by hitesh sir/React_project/Mega_Project/src/pages/HomePage.jsx
+++ react by hitesh sir/React_project/Mega_Project/src/pages/HomePage.jsx
@@ -11,11 +11,23 @@ function HomePage() {
             setPost(post.documents)
         }
       })
-    }, [third])
+    }, [])
     
-  return (
-    <div>HomePage</div>
-  )
+  if (Post.length === 0) {
+    return (
+      <div className='w-full py-8'>
+            <Container>
+                <div className='flex flex-wrap'>
+                    {Post.map((post) => (
+                        <div key={post.$id} className='p-2 w-1/4'>
+                            <PostCard {...post} />
+                        </div>
+                    ))}
+                </div>
+            </Container>
+        </div>
+    )
+  }
 }
 
 export default HomePage
---
+++ react by hitesh sir/React_project/Mega_Project/src/pages/Post.jsx
@@ -0,0 +1,14 @@
+import React, {useEffect, useState} from 'react'
+import appwriteService from '../appwrite/config'
+import { Button, Container } from '../Components'
+import { useParams } from 'react-router-dom'
+import parse from 'html-react-parser'
+import { useSelector } from 'react-redux'
+
+function post() {
+  return (
+    <div>post</div>
+  )
+}
+
+export default post
