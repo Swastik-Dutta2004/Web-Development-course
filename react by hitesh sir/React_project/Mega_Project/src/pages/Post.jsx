b358161b4a9ac16fa7e1846c57eb22398bac2868
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-10-18 10:36:00 +0530

    5th commit

--- react by hitesh sir/React_project/Mega_Project/src/pages/Post.jsx
+++ react by hitesh sir/React_project/Mega_Project/src/pages/Post.jsx
@@ -1,11 +1,33 @@
 import React, {useEffect, useState} from 'react'
 import appwriteService from '../appwrite/config'
 import { Button, Container } from '../Components'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate, Link } from 'react-router-dom'
 import parse from 'html-react-parser'
 import { useSelector } from 'react-redux'
 
 function post() {
+    const [Post, setPost] = useState(null)
+    const {slug} = useParams();
+    const navigate = useNavigate()
+
+    const userData = useSelector((state) => state.auth.userData)
+    const isAuthor = Post && userData ? Post.id === userData.$id : false
+
+    useEffect(() => {
+        if (slug) {
+            appwriteService.getPost(slug).then((post) => {
+                if (post) {
+                    setPost(post)
+                }else{
+                    navigate('/')
+                }
+            })
+        }else{
+            navigate('/')
+        }
+    }, [])
+    
+
   return (
     <div>post</div>
   )
