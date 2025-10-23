ae8937e479a64a74a6c08ea6c0bd35632c296dc1
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-10-23 20:32:10 +0530

    4th commit

--- react by hitesh sir/React_project/Mega_Project/src/Components/Header/LogoutBtn.jsx
+++ react by hitesh sir/React_project/Mega_Project/src/Components/Header/LogoutBtn.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {useDispatch} from 'react-redux'
-import authService from '../../appwrite/config'
+import authService from '../../appwrite/auth'
 import { logout } from '../../Store/authSlice'
 
 const LogoutBtn = () => {
@@ -11,7 +11,9 @@ const LogoutBtn = () => {
         })
     }
   return (
-    <button className='inline-back px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>Logout</button>
+    <button className='inline-back px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+    onClick={logoutHandler}
+    >Logout</button>
   )
 }
 
