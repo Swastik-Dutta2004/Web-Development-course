482d05497e1d67e8f8d3a6916cd6661ef0c8f503
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-10-25 20:08:42 +0530

    11t commit

--- React_jobs/src/App.jsx
+++ React_jobs/src/App.jsx
@@ -1,9 +1,14 @@
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import React from 'react'
 import HomePage from './Pages/HomePage'
+import MainLayout from './Layout/MainLayout'
 
 
-const router = createBrowserRouter(createRoutesFromElements(<Route index element={<HomePage/>} />))
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<MainLayout />}>
+    <Route index element={<HomePage />} />
+  </Route>
+))
 
 const App = () => {
   return (
