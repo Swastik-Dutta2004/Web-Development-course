12d21adcd626fb84ae2118a1978606a83b2654da
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-10-25 20:14:40 +0530

    12th commit

--- React_jobs/src/Pages/HomePage.jsx
+++ React_jobs/src/Pages/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Hero from '../Components/Hero'
+import HomeCard from '../Components/HomeCard'
+import JobListing from '../Components/JobListing'
 
 const HomePage = () => {
     return (
         <>
             <Hero />
+            <HomeCard/>
+            <JobListing/>
         </>
     )
 }
