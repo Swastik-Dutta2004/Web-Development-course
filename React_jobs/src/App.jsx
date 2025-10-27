b8999ab72f5e096498788a53d7e29c831dc2092b
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-10-27 09:18:01 +0530

    2nd commit

--- React_jobs/src/App.jsx
+++ React_jobs/src/App.jsx
@@ -11,15 +11,22 @@ import AddJobPage from './Pages/AddJobPage'
 
 const App = () => {
 
-  const addJob = (newJob) => {
-
+  const addJob = async (newJob) => {
+    const res = await fetch('api/jobs',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json', 
+      },
+      body: JSON.stringify(newJob)
+    })
+    return
   }
 
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
-      <Route path='/Add-job' element={<AddJobPage addJobSunmit={addJob}  />} />
+      <Route path='/Add-job' element={<AddJobPage addJobSunmit={addJob}/>} />
       <Route path='/jobs/:id' element={<JobPage />} loader={JobLoader} />
       <Route path='*' element={<NotFoundPage />} />
     </Route>
