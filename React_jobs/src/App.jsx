94fe3a06f43e79882de53622e44ce525f508d607
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-10-27 08:46:37 +0530

    today 1st commit

--- React_jobs/src/App.jsx
+++ React_jobs/src/App.jsx
@@ -4,21 +4,27 @@ import HomePage from './Pages/HomePage'
 import MainLayout from './Layout/MainLayout'
 import JobsPage from './Pages/JobsPage'
 import NotFoundPage from './Pages/NotFoundPage'
-import JobPage, {JobLoader} from './Pages/JobPage'
+import JobPage, { JobLoader } from './Pages/JobPage'
 import AddJobPage from './Pages/AddJobPage'
 
 
-const router = createBrowserRouter(createRoutesFromElements(
-  <Route path='/' element={<MainLayout />}>
-    <Route index element={<HomePage />} />
-    <Route path='/jobs' element = {<JobsPage/>}/>
-    <Route path='/Add-job' element = {<AddJobPage/>}/>
-    <Route path='/jobs/:id' element = {<JobPage/>} loader= {JobLoader}/>
-    <Route path='*' element = {<NotFoundPage/>}/>
-  </Route>
-))
 
 const App = () => {
+
+  const addJob = (newJob) => {
+
+  }
+
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='/Add-job' element={<AddJobPage addJobSunmit={addJob}  />} />
+      <Route path='/jobs/:id' element={<JobPage />} loader={JobLoader} />
+      <Route path='*' element={<NotFoundPage />} />
+    </Route>
+  ))
+
   return (
     <RouterProvider router={router} />
   )
