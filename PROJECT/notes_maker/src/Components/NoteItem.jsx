6dd7f071f5cbf541f935b3e2a4c47ffea5794c7b
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-10-29 17:14:31 +0530

    8th commit

--- PROJECT/notes_maker/src/Components/NoteItem.jsx
+++ PROJECT/notes_maker/src/Components/NoteItem.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
 function NoteItem({notes, deleteNote}) {
+  
+  
   return (
-    <div>NoteItem</div>
+    <div className="p-4 rounded-lg shadow-md flex justify-between items-start"
+      style={{ backgroundColor: notes.color }}>
+        <p className="font-medium text-gray-800">{notes.text}</p>
+        <button
+        onClick={() => deleteNote(notes.id)}
+        className="text-red-600 font-bold ml-2"
+        > ✖ </button>
+      </div>
   )
 }
 
