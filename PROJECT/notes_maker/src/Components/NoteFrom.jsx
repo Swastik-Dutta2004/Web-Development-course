60e09f85c3ae766e785fe7b4a51fe5987371f6b0
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-10-29 10:58:21 +0530

    4th commit

--- PROJECT/notes_maker/src/Components/NoteFrom.jsx
+++ PROJECT/notes_maker/src/Components/NoteFrom.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const NoteFrom = () => {
-  return (
-    <div>NoteFrom</div>
-  )
+    const [Notes, setNotes] = useState("")
+    const [Color, setColor] = useState("#fef08a")
+
+    const HandleSubmit = 
+
+    return (
+        <form action="">
+            <input type="text"
+                placeholder='Add your note'
+                value={Notes}
+                onChange={(e) => setNotes(e.target.value)}
+                />
+
+            <input type="color"
+                value={Color}
+                onChange={(e) => setColor(e.target.value)}
+            />
+            <button onClick={HandleSubmit}>Submit</button>
+        </form>
+    )
 }
 
 export default NoteFrom
