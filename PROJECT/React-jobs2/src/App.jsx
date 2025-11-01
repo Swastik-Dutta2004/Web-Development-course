bf084bf91f191f60478f20121cf091455f17b386
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-11-01 11:41:13 +0530

    5th commit

--- PROJECT/React-jobs2/src/App.jsx
+++ PROJECT/React-jobs2/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
+import HomeCard from './Components/HomeCard'
 
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <div>
       <Navbar/>
       <Hero title = "Becoming a web Developer" subtitle = "Just fucking Do it"/>
+      <HomeCard/>
     </div>
   )
 }
