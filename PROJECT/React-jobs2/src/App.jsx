239a8305a5b5572b30a0b603aaf6e5be8a3b9327
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-11-06 21:34:54 +0530

    3rd commit

--- PROJECT/React-jobs2/src/App.jsx
+++ PROJECT/React-jobs2/src/App.jsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter(createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
-      <Route path='/job/:id' element={<JobPage />} />
+      <Route path='/jobs/:id' element={<JobPage />} />
       <Route path='*' element={<NotFoundPage />} />
     </Route>
   )
--- PROJECT/React-jobs2/src/Page/JobPage.jsx
+++ PROJECT/React-jobs2/src/Page/JobPage.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import Spinner from '../Components/Spinner'
 
 const JobPage = () => {
-  return (
-    <div>JobPage</div>
-  )
+    const [Jobs, setJobs] = useState(null)
+    const { id } = useParams()
+    const [Loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchJob = async () => {
+            try {
+                const res = await fetch(`/api/jobs/${id}`)
+                const data = await res.json()
+                setJobs(data)
+            } catch (error) {
+                console.error("Data not found", error);
+            }
+            finally {
+                setLoading(false)
+            }
+        }
+        fetchJob()
+    }, [])
+
+    return (
+        <div>JobPage</div>
+    )
 }
 
 export default JobPage
