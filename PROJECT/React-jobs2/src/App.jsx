5ff7f5d8111895506da54ca077afd68e20702719
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-11-06 21:49:00 +0530

    4th commit

--- PROJECT/React-jobs2/src/App.jsx
+++ PROJECT/React-jobs2/src/App.jsx
@@ -8,13 +8,13 @@ import MainLayout from './layout/MainLayout'
 import HomePage from './Page/HomePage'
 import JobsPage from './Page/JobsPage'
 import NotFoundPage from './Page/NotFoundPage'
-import JobPage from './Page/JobPage'
+import JobPage , {jobLoader} from './Page/JobPage'
 
 const router = createBrowserRouter(createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
-      <Route path='/jobs/:id' element={<JobPage />} />
+      <Route path='/jobs/:id' element={<JobPage />} loader= {jobLoader} />
       <Route path='*' element={<NotFoundPage />} />
     </Route>
   )
--- PROJECT/React-jobs2/src/Page/JobPage.jsx
+++ PROJECT/React-jobs2/src/Page/JobPage.jsx
@@ -1,31 +1,36 @@
-import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import Spinner from '../Components/Spinner'
 
 const JobPage = () => {
-    const [Jobs, setJobs] = useState(null)
+    // const [Jobs, setJobs] = useState(null)
+    // const [Loading, setLoading] = useState(true)
     const { id } = useParams()
-    const [Loading, setLoading] = useState(true)
 
-    useEffect(() => {
-        const fetchJob = async () => {
-            try {
-                const res = await fetch(`/api/jobs/${id}`)
-                const data = await res.json()
-                setJobs(data)
-            } catch (error) {
-                console.error("Data not found", error);
-            }
-            finally {
-                setLoading(false)
-            }
-        }
-        fetchJob()
-    }, [])
+    // useEffect(() => {
+    //     const fetchJob = async () => {
+    //         try {
+    //             const res = await fetch(`/api/jobs/${id}`)
+    //             const data = await res.json()
+    //             setJobs(data)
+    //         } catch (error) {
+    //             console.error("Data not found", error);
+    //         }
+    //         finally {
+    //             setLoading(false)
+    //         }
+    //     }
+    //     fetchJob()
+    // }, [])
 
     return (
-        <div>JobPage</div>
+        Loading ? <Spinner/> : <div>{Jobs.title}</div>
     )
 }
 
-export default JobPage
+const jobLoader = async ({params}) => {
+    const res = await fetch(`/api/jobs/${params.id}`)
+    const data = await res.json()
+    return data
+}
+
+export {JobPage as default, jobLoader}
