4ed5102882d29718aa0354ce28e79038541b8ae2
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-11-07 09:02:29 +0530

    2nd commit

--- PROJECT/React-jobs2/src/Page/JobPage.jsx
+++ PROJECT/React-jobs2/src/Page/JobPage.jsx
@@ -1,6 +1,6 @@
 import { useParams, useLoaderData } from 'react-router-dom'
 import { Link } from 'react-router-dom'
-import { FaMapMarked,  FaArrowLeft } from 'react-icons/fa'
+import { FaMapMarker,  FaArrowLeft } from 'react-icons/fa'
 const JobPage = () => {
     // const [Jobs, setJobs] = useState(null)
     // const [Loading, setLoading] = useState(true)
@@ -10,7 +10,7 @@ const JobPage = () => {
     // useEffect(() => {
     //     const fetchJob = async () => {
     //         try {
-    //             const res = await fetch(`/api/jobs/${id}`)
+    //             const res = await fetch(`/api/Jobs/${id}`)
     //             const data = await res.json()
     //             setJobs(data)
     //         } catch (error) {
@@ -28,7 +28,7 @@ const JobPage = () => {
             <section>
                 <div className='container m-auto py-6 px-6'>
                     <Link
-                        to='/jobs'
+                        to='/Jobs'
                         className='text-indigo-500 hover:text-indigo-600 flex items-center'
                     >
                         <FaArrowLeft className='mr-2' /> Back to Job Listings
@@ -41,11 +41,11 @@ const JobPage = () => {
                     <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
                         <main>
                             <div className='bg-white p-6 rounded-lg shadow-md text-center md:text-left'>
-                                <div className='text-gray-500 mb-4'>{jobs.type}</div>
-                                <h1 className='text-3xl font-bold mb-4'>{jobs.title}</h1>
+                                <div className='text-gray-500 mb-4'>{Jobs.type}</div>
+                                <h1 className='text-3xl font-bold mb-4'>{Jobs.title}</h1>
                                 <div className='text-gray-500 mb-4 flex align-middle justify-center md:justify-start'>
                                     <FaMapMarker className='text-orange-700 mr-1' />
-                                    <p className='text-orange-700'>{jobs.location}</p>
+                                    <p className='text-orange-700'>{Jobs.location}</p>
                                 </div>
                             </div>
 
@@ -54,52 +54,50 @@ const JobPage = () => {
                                     Job Description
                                 </h3>
 
-                                <p className='mb-4'>{jobs.description}</p>
+                                <p className='mb-4'>{Jobs.description}</p>
 
                                 <h3 className='text-indigo-800 text-lg font-bold mb-2'>
                                     Salary
                                 </h3>
 
-                                <p className='mb-4'>{jobs.salary} / Year</p>
+                                <p className='mb-4'>{Jobs.salary} / Year</p>
                             </div>
                         </main>
 
-                        {/* <!-- Sidebar --> */}
                         <aside>
                             <div className='bg-white p-6 rounded-lg shadow-md'>
                                 <h3 className='text-xl font-bold mb-6'>Company Info</h3>
 
-                                <h2 className='text-2xl'>{jobs.company.name}</h2>
+                                <h2 className='text-2xl'>{Jobs.company.name}</h2>
 
-                                <p className='my-2'>{jobs.company.description}</p>
+                                <p className='my-2'>{Jobs.company.description}</p>
 
                                 <hr className='my-4' />
 
                                 <h3 className='text-xl'>Contact Email:</h3>
 
                                 <p className='my-2 bg-indigo-100 p-2 font-bold'>
-                                    {jobs.company.contactEmail}
+                                    {Jobs.company.contactEmail}
                                 </p>
 
                                 <h3 className='text-xl'>Contact Phone:</h3>
 
                                 <p className='my-2 bg-indigo-100 p-2 font-bold'>
                                     {' '}
-                                    {jobs.company.contactPhone}
+                                    {Jobs.company.contactPhone}
                                 </p>
                             </div>
 
                             <div className='bg-white p-6 rounded-lg shadow-md mt-6'>
                                 <h3 className='text-xl font-bold mb-6'>Manage Job</h3>
                                 <Link
-                                    to={`/edit-jobs/${jobs.id}`}
+                                    to={`/edit-Jobs/${Jobs.id}`}
                                     className='bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block'
                                 >
                                     Edit Job
                                 </Link>
                                 <button
-                                    onClick={() => onDeleteClick(jobs.id)}
-                                    className='bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block'
+                                    
                                 >
                                     Delete Job
                                 </button>
@@ -114,7 +112,7 @@ const JobPage = () => {
 }
 
 const jobLoader = async ({ params }) => {
-    const res = await fetch(`/api/jobs/${params.id}`)
+    const res = await fetch(`/api/Jobs/${params.id}`)
     const data = await res.json()
     return data
 }
